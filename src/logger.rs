3a09cf6db8e10a1ae6a02e44475da7610c85b949
Batchhh <Noslawo@proton.me>
2026-03-10 16:36:48 +0100

    added logger

--- src/logger.rs
+++ src/logger.rs
@@ -1,3 +1,13 @@
-pub fn info(_msg: &str) {}
-pub fn error(_msg: &str) {}
-pub fn warning(_msg: &str) {}
+const TAG: &str = "il2cpp_resolver";
+
+pub fn info(msg: &str) {
+    eprintln!("[{}] [INFO] {}", TAG, msg);
+}
+
+pub fn error(msg: &str) {
+    eprintln!("[{}] [ERROR] {}", TAG, msg);
+}
+
+pub fn warning(msg: &str) {
+    eprintln!("[{}] [WARN] {}", TAG, msg);
+}
